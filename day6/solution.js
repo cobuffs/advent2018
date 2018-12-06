ef29e0b3abda81a652ae71ad36d44af7abdb18fa
Jeremy Lizza <jeremyl@Lizza-Macbook.local>
2018-12-06 14:55:12 -0500

    made it better

--- day6/solution.js
+++ day6/solution.js
@@ -6,12 +6,10 @@ function sortNumber(a, b) {
 }
 
 //build a grid of all coordinates
-var map = new Array(400);
-for(var i = 0; i < 400; i++) {
-    map[i] = new Array(400);
-}
 var knownpoints = new Map();
 var i = 0;
+var maxx = 0;
+var maxy = 0;
 (inputs).forEach(input => {
     //kill the space
     input = input.trim();
@@ -20,16 +18,23 @@ var i = 0;
     var x = parseInt(coords[0]);
     var y = parseInt(coords[1]);
     knownpoints.set(i, {"id": i++, "x": x, "y": y, "area":0})
+    maxx = x > maxx ? x : maxx;
+    maxy = y > maxy ? y : maxy;
 
     //map[x][y] = {"id": i++, "x": x, "y": y, "area":0};
 });
 //build a grid of every point
+var map = new Array(maxx);
+for(var i = 0; i < maxx; i++) {
+    map[i] = new Array(maxy);
+}
+
 //where it isn't a known point check the distance again every known point
 //for the shortest distance, log it
 var regionsum = 0;
-for(var i = 0; i < 400; i++)
+for(var i = 0; i < maxx; i++)
 {
-    for(var j = 0; j < 400; j++) {
+    for(var j = 0; j < maxy; j++) {
         if(typeof map[i][j] == 'undefined') {
             distances = [];
             var distsum = 0;
@@ -52,8 +57,8 @@ for(var i = 0; i < 400; i++)
     }
 }
 
-for(var i = 0; i < 400; i++) {
-    for(var j = 0; j < 400; j++) {
+for(var i = 0; i < maxx; i++) {
+    for(var j = 0; j < maxy; j++) {
         var element = map[i][j];
         if (element.length == 1) {
             var pointid = element[0].point.id;
@@ -63,20 +68,20 @@ for(var i = 0; i < 400; i++) {
 }
 
 //bottom
-for(var i = 0; i < 400; i++) {
+for(var i = 0; i < maxx; i++) {
     var point = map[i][0][0].point;
     knownpoints.delete(point.id);
 }
-for(var i = 1; i < 400; i++) {
+for(var i = 1; i < maxy; i++) {
     var point = map[0][i][0].point;
     knownpoints.delete(point.id);
 }
-for(var i = 0; i < 400; i++) {
-    var point = map[399][i][0].point;
+for(var i = 0; i < maxy; i++) {
+    var point = map[maxx - 1][i][0].point;
     knownpoints.delete(point.id);
 }
-for(var i = 1; i < 400; i++) {
-    var point = map[i][399][0].point;
+for(var i = 1; i < maxx; i++) {
+    var point = map[i][maxy-1][0].point;
     knownpoints.delete(point.id);
 }
 
